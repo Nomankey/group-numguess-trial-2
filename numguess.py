085901b422fabc65145c74c05a39a743200881db
ekfka4863 <ekfka4863@gmail.com>
2021-01-31 01:14:57 +0900

    Solve: #2
    feat: Add elif statement
    elif statement print 'Up' when answer > guess

--- numguess.py
+++ numguess.py
@@ -1,7 +1,10 @@
 import random
 
+
 answer = random.randint(1, 100)
 guess = int(input('Enter Your Guess!: '))
 
 if answer == guess: 
-  print('Congratulations')
+    print('Congratulations')
+elif answer > guess:          # if answer == guess가 아닐 경우'Up'을 출력 
+   print('Up!')           
