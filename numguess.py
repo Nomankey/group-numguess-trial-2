27e4e9a0c28d510d28e7cb0867978add23ae7f42
ekfka4863 <ekfka4863@gmail.com>
2021-02-01 09:26:59 +0900

    Solved: #5
    feat: Add elif-statement
    
    elif > answer > guess: print('Up')

--- numguess.py
+++ numguess.py
@@ -6,5 +6,5 @@ guess = int(input('Enter Your Guess!: '))
 
 if answer == guess: 
     print('Congratulations')
-elif answer > guess:         # Add elif -> if answer == guess가 아닐 때'Up'을 출력한다 
+elif answer > guess:         # Add elif -> if answer == guess가 아닐 때'Up'을 print! 
    print('Up!')           
